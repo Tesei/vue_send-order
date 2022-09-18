d4ca2b89c28ea338891b0cc367c870fc7310e50d
Stepan G <gud-stepan@yandex.ru>
2022-09-18 18:44:42 +0300

    clear trash files

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,3 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-// module.exports = {
-  // Подключение общих стилей для всех компонентов:
-  // css: {
-  //   loaderOptions: {
-  //     sass: {
-  //       additionalData: `@import "@/styles/index.scss";`
-  //     }
-  //   }
-  // }
-// }
